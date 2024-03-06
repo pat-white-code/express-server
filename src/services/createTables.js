4b9660394a8ad6a1e0646c9fb134d8280e200a9a
Patrick White <patrickwhite@Patricks-MacBook-Pro.local>
2024-03-05 19:43:57 -0600

    feat: add cards table

--- src/services/createTables.js
+++ src/services/createTables.js
@@ -7,14 +7,34 @@ const imagesTable = `CREATE TABLE IF NOT EXISTS
         cloudinary_id VARCHAR(128) NOT NULL,
         image_url VARCHAR(128) NOT NULL
     )`;
+
+const cardsTable = `CREATE TABLE IF NOT EXISTS
+cards(
+    id UUID PRIMARY KEY,
+    multiverseId INT,
+    name VARCHAR(250) NOT NULL,
+    rarity VARCHAR(50) NOT NULL,
+    image_url VARCHAR(250) NOT NULL
+);`;
+
 console.log('creating images table...')
 pool.query(imagesTable)
     .then((res) => {
         console.log('createTables response', res)
-        pool.end()
     })
     .catch((err) => {
         console.log('aborting images table...')
+        console.log(err)
+        pool.end()
+    })
+
+pool.query(cardsTable)
+    .then((res) => {
+        console.log('cardsTable response', res)
+        pool.end()
+    })
+    .catch((err) => {
+        console.log('aborting cardsTable...')
         console.log(err)
         pool.end()
     })
